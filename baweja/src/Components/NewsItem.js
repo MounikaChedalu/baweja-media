527d347907d276d9f8755443c309d20dc7138e7e
MounikaChedalu <mounikachedalu01@gmail.com>
2023-12-09 19:14:20 +0530

    files created

--- baweja/src/Components/NewsItem.js
+++ baweja/src/Components/NewsItem.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 
 const NewsItem = ({ article }) => {
-  const { title, description, url, publishedAt } = article;
+  const { title, description, url, publishedAt,source,author} = article;
 
   return (
     <div>
+      <h2>Source:{source.name}</h2>
       <h2>{title}</h2>
+      <h3>author:{author}</h3>
       <p>{description}</p>
       <p>Published at: {publishedAt}</p>
       <a href={url} target="_blank" rel="noopener noreferrer">
